3d8e208590281e2bb5fce84f17745202233aa0ea
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2023-06-13 17:05:25 +0200

    handle search token search and state

--- src/app/TradingPairView/page.tsx
+++ src/app/TradingPairView/page.tsx
@@ -14,10 +14,12 @@ interface tokensDataType{
 }
 
 export default function TradingPairView(){
-    const [keyPairs,setKeyPairs] = useState({
-        token_1:"",
-        token_2:""
-    })
+    const [token_1,setToken_1]=useState<string>("")
+    const [token_2,setToken_2]=useState<string>("")
+    // const [keyPairs,setKeyPairs] = useState({
+    //     token_1:"",
+    //     token_2:""
+    // })
     const [hide,setHide] = useState({
         token_1:true,
         token_2:true
@@ -25,14 +27,43 @@ export default function TradingPairView(){
     const [tokens,setTokens] = useState<Array<tokensDataType>>()
 
     const searchTokens=(value:string,display:boolean)=>{
+        //display parameter used to choose where to show token search results
         value = value.trim()
         const search_data:any = search(data,["name","symbol"],value)
         if(value.length>3 && display === true && search_data.length>0){
         setTokens(search_data)
         setHide({...hide,token_1:false})
+        }else if(value.length>3 && display === false && search_data.length>0){
+            setTokens(search_data)
+            setHide({...hide,token_2:false})
         }else{
             setHide({...hide,token_1:true,token_2:true})
         }
+    }
+
+    const swapTokens=()=>{
+        const copyCurrentTokens = {token_1,token_2}
+        setToken_1(copyCurrentTokens.token_2)
+        setToken_2(copyCurrentTokens.token_1)
+    }
+
+    const ViewResults=()=>{
+        tokens?.map((item:tokensDataType,index:number)=>{
+            return(
+                <div key={index} className="flex flex-row m-4 justify-between ">
+                    <div>
+                        <img src={item.logoURI} width="20px" className="rounded" alt={`image of ${item.name} logo`}/>
+                    </div>
+                  
+                    <div>
+                    <p>{item.symbol}</p>
+                    </div>
+                      <div>
+                    <p>{item.name}</p>
+                    </div>         
+                </div>
+            )
+        })
     }
     return(
         <div className="container">
@@ -43,13 +74,13 @@ export default function TradingPairView(){
                 </div>
                 <div className="mb-5">
                     <div>
-                        <input className={generalInput}placeholder="You Pay" onChange={(e)=>searchTokens(e.target.value,true)}/>
+                        <input className={generalInput}placeholder="Paying Token" onChange={(e)=>{setToken_1(e.target.value);searchTokens(e.target.value,true)}} value={token_1}/>
                     </div>
-                    <div hidden={hide.token_1} className="absolute max-h-52 min-w-[30%] bg-white shadow-lg overflow-y-auto p-5 ">
+                    <div hidden={hide.token_1} className="absolute max-h-52 min-w-[30%] bg-white shadow overflow-y-auto p-5  ">
                         {
                             tokens?.map((item:tokensDataType,index:number)=>{
                                 return(
-                                    <div key={index} className="flex flex-row m-4 justify-between ">
+                                    <div key={index} className="flex flex-row m-4 justify-between cursor-pointer" onClick={()=>{setToken_1(item.symbol);setHide({...hide,token_1:true})}}>
                                         <div>
                                             <img src={item.logoURI} width="20px" className="rounded" alt={`image of ${item.name} logo`}/>
                                         </div>
@@ -71,13 +102,34 @@ export default function TradingPairView(){
                 </div>
                 <div className="m-2 text-center">
                    
-                    <button data-tip-content="Swap Tokens"  data-tip-id="tokens"  data-tooltip-place="top" className={generalBtn}><IonIcon icon={swapVertical}/></button>
+                    <button data-tip-content="Swap Tokens"  data-tip-id="tokens"  data-tooltip-place="top" className={generalBtn} onClick={()=>swapTokens()}><IonIcon icon={swapVertical}/></button>
                     <Tooltip id="tokens"/>
                 </div>
                 <div className="mb-5">
                  
                     <div>
-                        <input className={generalInput} placeholder="You receive"/>
+                        <input className={generalInput} placeholder="Receiving Token" onChange={(e)=>{setToken_2(e.target.value);searchTokens(e.target.value,false)}} value={token_2}/>
+                    </div>
+                    <div hidden={hide.token_2} className="absolute max-h-52 min-w-[30%] bg-white shadow-lg overflow-y-auto p-5 cursor-pointer">
+                        {
+                            tokens?.map((item:tokensDataType,index:number)=>{
+                                return(
+                                    <div key={index} className="flex flex-row m-4 justify-between " onClick={()=>{setToken_2(item.symbol);setHide({...hide,token_2:true})}}>
+                                        <div>
+                                            <img src={item.logoURI} width="20px" className="rounded" alt={`image of ${item.name} logo`}/>
+                                        </div>
+                                      
+                                        <div>
+                                        <p>{item.symbol}</p>
+                                        </div>
+                                          <div>
+                                        <p>{item.name}</p>
+                                        </div>
+                               
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
