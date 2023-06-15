53017767507e9f30890cbb72d310d382bfd37737
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2023-06-15 22:01:29 +0200

    boostrap style additions

--- src/app/components/cssStyles.ts
+++ src/app/components/cssStyles.ts
@@ -1,5 +1,5 @@
 export const generalBtn:string = "btnPrimary rounded-full font-semibold"
-export const generalInput:string = "rounded-full inputPrimary p-3"
+export const generalInput:string = "rounded inputPrimary p-3"
 
-export const generalModal:string = "shadow rounded-2xl p-5  mb-5 bg-slate-100"
+export const generalModal:string = "shadow rounded p-5  mb-5 bg-slate-100"
 export const searchTokenDropdown:string = "absolute bg-white shadow overflow-y-auto mb-5 p-3  rounded"
