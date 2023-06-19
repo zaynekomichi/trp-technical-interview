460b43d4f160356f7bae2e79d45554a7504d5675
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2023-06-19 14:07:55 +0200

    code cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   })
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className}  suppressHydrationWarning={true}>
         <TokenContext.Provider value={{currentTokens,setCurrentTokens}} className="container">
         <TradingPairView/>
         {children}
